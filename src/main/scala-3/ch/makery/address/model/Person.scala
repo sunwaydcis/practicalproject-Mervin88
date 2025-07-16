1e9cb243ba966f73ca90385d8814c2d5352f962a
Mervin88 <tanmervin040803@gmail.com>
2025-07-16 20:56:02 +0800

    Update Person.scala

--- src/main/scala-3/ch/makery/address/model/Person.scala
+++ src/main/scala-3/ch/makery/address/model/Person.scala
@@ -8,6 +8,6 @@ class Person ( firstNameS : String, lastNameS : String )://6 properties
   var lastName   = new StringProperty(lastNameS)
   var street     = new StringProperty("some Street")
   //var postalCode = IntegerProperty(1234)
-  var postalCode = ObjectProperty[Int](1234)
+  var postalCode = ObjectProperty[Int](1234) //companion object
   var city       = new StringProperty("some city")
   var date       = ObjectProperty[LocalDate](LocalDate.of(1999, 2, 21))
