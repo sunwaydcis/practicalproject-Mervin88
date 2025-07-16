6aeb0584d5dffd36e352bc9a5f09f6a5549ee56d
Mervin88 <tanmervin040803@gmail.com>
2025-07-16 20:56:09 +0800

    Update MainApp.scala

--- src/main/scala-3/ch/makery/address/MainApp.scala
+++ src/main/scala-3/ch/makery/address/MainApp.scala
@@ -1,11 +1,14 @@
 package ch.makery.address
 
+import ch.makery.address.model.Person
 import javafx.fxml.FXMLLoader
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.Includes.*
 import javafx.scene as jfxs
+import scalafx.beans.property.StringProperty
+import scalafx.collections.ObservableBuffer
 
 object MainApp extends JFXApp3:
 
@@ -40,4 +43,52 @@ object MainApp extends JFXApp3:
     val roots = loader.getRoot[jfxs.layout.AnchorPane] //load anchor pane
     this.roots.get.center = roots //set to the center
 
-  var personA = 
+  //String Property have publisher (data) & subscriber (require data)
+  val stringA = new StringProperty("Hello") //publisher
+  val stringB = new StringProperty("Sunway") //subscriber
+  val stringC = new StringProperty("University") //subscriber
+
+  //binding
+  stringA.value = "world"
+  stringB <== stringA //value of string A will push to string B
+  stringC <== stringA
+  //stringB.value = "google" //all will change to this
+
+  //println(stringA.value)
+
+  stringA.onChange { (_, oldvalue, newvalue) =>
+    println(s"stringA change from $oldvalue to $newvalue"
+    )
+  }
+
+  stringA.onChange { (_, _, _) =>
+    println(s"stringA has change !!!!!!!!"
+    )
+  }
+
+  stringA.value = "Google"
+
+  //Function Object, Function has type
+  val func1 = (a:Int) =>  // val func1: Int => Int = (a) => //input is int, output also int
+    a + 1
+  //println(func1(8))
+
+
+  /**
+   * The data as an observable list of Persons.
+   */
+  val personData = new ObservableBuffer[Person]() //buffer is mutable, everytime add smtg the buffer will change
+
+  /**
+   * Constructor
+   */
+  personData += new Person("Hans", "Muster") //+= add 1 value to the collection; ++= add collection to collection
+  personData += new Person("Ruth", "Mueller")
+  personData += new Person("Heinz", "Kurz")
+  personData += new Person("Cornelia", "Meier")
+  personData += new Person("Werner", "Meyer")
+  personData += new Person("Lydia", "Kunz")
+  personData += new Person("Anna", "Best")
+  personData += new Person("Stefan", "Meier")
+  personData += new Person("Martin", "Mueller")
+   
